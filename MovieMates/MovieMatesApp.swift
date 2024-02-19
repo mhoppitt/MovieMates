bce11af102ec9ee755f63658f48641979d5d8aa1
mhoppitt <mhoppitt18@outlook.com>
2024-02-19 17:22:34 +1100

    Initial Commit

---
+++ MovieMates/MovieMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatesApp.swift
+//  MovieMates
+//
+//  Created by Matt Hoppitt on 19/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
